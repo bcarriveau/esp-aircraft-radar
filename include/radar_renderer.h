1747cb169969dff12f3a5d794f561b56dc2acc83
Bill C <Bcarriveau@gmail.com>
2026-07-23 20:38:04 -0500

    Product 31: Add aircraft bitmaps and nearest heading arrow
    - Add aircraft-type bitmaps to radar side panels and nearest lists
    - Add a rotating heading arrow to the idle nearest-aircraft card
    - Keep independent heading-arrow point storage
    - Preserve 200-target PSRAM ownership and networking behavior
    
    Build: 7IN-20260723-PRODUCT31-NEAREST-HEADING-ARROW

--- include/radar_renderer.h
+++ include/radar_renderer.h
@@ -11,6 +11,8 @@ constexpr int WIDTH = 430;
 constexpr int HEIGHT = 360;
 constexpr int PREVIEW_WIDTH = 220;
 constexpr int PREVIEW_HEIGHT = 150;
+constexpr int SIDE_ICON_WIDTH = 28;
+constexpr int SIDE_ICON_HEIGHT = 19;
 
 struct View {
   lv_obj_t* canvas = nullptr;
@@ -19,13 +21,21 @@ struct View {
   lv_obj_t* leftNearestModeLabel = nullptr;
   lv_obj_t* leftNearestCallsignLabel = nullptr;
   lv_obj_t* leftNearestSummaryLabel = nullptr;
+  lv_obj_t* leftNearestIcon = nullptr;
+  lv_color_t* leftNearestIconBuffer = nullptr;
+  lv_obj_t* leftNearestHeadingArrow = nullptr;
+  lv_obj_t* leftNearestHeadingLabel = nullptr;
   lv_obj_t* aircraftModeLabel = nullptr;
   lv_obj_t* nearestCallsignLabel = nullptr;
   lv_obj_t* nearestSummaryLabel = nullptr;
+  lv_obj_t* priorityIcon = nullptr;
+  lv_color_t* priorityIconBuffer = nullptr;
   lv_obj_t* headingArrow = nullptr;
   lv_obj_t* headingLabel = nullptr;
   char* leftNearestHex = nullptr;
   lv_obj_t* listLabels[5]{};
+  lv_obj_t* listIcons[5]{};
+  lv_color_t* listIconBuffers[5]{};
   char* listHexes[5]{};
 };
 
@@ -42,6 +52,8 @@ bool hitTest(int canvasX, int canvasY, HitResult& result);
 
 void drawAircraftPreview(lv_obj_t* canvas, lv_color_t* buffer,
                          const aircraft::Target& target);
+void drawSideBitmapIcon(lv_obj_t* canvas, lv_color_t* buffer,
+                        AircraftBitmapId bitmapId);
 void drawTrackBitmapIcon(lv_draw_ctx_t* drawContext, int centerX, int centerY,
                          AircraftBitmapId bitmapId);
 
